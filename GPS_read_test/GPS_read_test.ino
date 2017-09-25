89a61ef51f7ad8c87730808eb11de3777e5de17a
Unknown <antoinelemarchand@gmail.com>
2017-09-25 15:17:59 +0200

    gps read

---
+++ GPS_read_test/GPS_read_test.ino
@@ -0,0 +1,199 @@
+#include <RingBuf.h>
+
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include "math.h"
+
+int i,j,x;
+uint8_t gps_msg[44];
+uint8_t gps_header[2];
+int32_t int32_buffer;
+unsigned char *ptr_int32_buffer = (unsigned char *)&int32_buffer;
+uint32_t uint32_buffer;
+unsigned char *ptr_uint32_buffer = (unsigned char *)&uint32_buffer;
+
+float NED_coordinates[3];
+float NED_coordinates_accuracy[3];
+float NED_speed[3];
+float NED_acc[3];
+
+// for BNO
+Adafruit_BNO055 bno = Adafruit_BNO055();
+uint8_t sys, gyr, accel, mag = 0;
+float quaternion[4], proper_acc[3];
+
+uint32_t last_time,time_since_last;
+uint32_t time_period_us = 10000;
+
+uint32_t accuracy_mask;
+
+int8_t expected_size = 40;
+
+uint8_t first_update, header_read;
+
+//////////////////////////////////////////////////////////////////////
+
+// Projection of a vector using a quaternion
+// input: q[4]: quaternion in flt
+// input: v_in[3]: vector to be projected in the new base in flt
+// output: v_out[3]: v_in rotated thanks to quaternion input
+
+void vector_quat_proj(float q[4], float v_in[3], float v_out[3])
+{
+  v_out[0] = (1 - 2*q[2]*q[2] - 2*q[3]*q[3]) * v_in[0] + (2*q[1]*q[2] - 2*q[3]*q[0]) * v_in[1]     + (2*q[1]*q[3] + 2*q[2]*q[0]) * v_in[2];
+  v_out[1] = (2*q[1]*q[2] + 2*q[3]*q[0]) * v_in[0]     + (1 - 2*q[1]*q[1] - 2*q[3]*q[3]) * v_in[1] + (2*q[2]*q[3] - 2*q[1]*q[0]) * v_in[2];
+  v_out[2] = (2*q[1]*q[3] - 2*q[2]*q[0]) * v_in[0]     + (2*q[2]*q[3] + 2*q[1]*q[0]) * v_in[1]     + (1 - 2*q[1]*q[1] - 2*q[2]*q[2]) * v_in[2];
+}
+
+/////////////////////////////////////////////////////////////////////
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(57600);
+
+  if(!bno.begin(Adafruit_BNO055::OPERATION_MODE_NDOF))
+  {
+    // There was a problem detecting the BNO055 ... check your connections 
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  //RingBuf *RingBuf_new(1, 128);
+
+  Serial.println("Start");  
+  
+  i = 0;
+  last_time = micros();
+  first_update = 1;
+  header_read = 0;
+}
+
+/////////////////////////////////////////////////////////////////////
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(header_read==0 && Serial.available()>3)
+  {
+    x = Serial.read();
+    if (x==0xB5)
+    {
+      x = Serial.read();
+      if (x==0x62)
+      {
+        Serial.readBytes(gps_header,2);
+
+        Serial.print("Head");Serial.print(" ");Serial.println(Serial.available());
+        
+        if (gps_header[0] == 0x01 && gps_header[1] == 0x3C )
+        { // position
+          expected_size = 42;
+          header_read = 1;
+        } else if (gps_header[0] == 0x01 && gps_header[1] == 0x12 )
+        { // speed
+          expected_size = 38;
+          header_read = 1;
+        }
+      }
+    }
+  }
+
+  if(header_read==1 && Serial.available()>expected_size-1)
+  {
+    Serial.print(Serial.available());Serial.print(" ");
+    header_read = 0;
+    if (gps_header[0] == 0x01 && gps_header[1] == 0x3C )
+    { // position
+      Serial.readBytes(gps_msg,42);
+
+      Serial.println("pos");
+      
+      for(i=0;i<3;i++)
+      {
+        for(j=0;j<4;j++)
+        {
+          ptr_int32_buffer[j] = gps_msg[4*i+j+10];
+        }
+        NED_coordinates[i] = ((float)(int32_buffer)+((float)gps_msg[i+22]*0.01));
+        //Serial.print(NED_coordinates[i]);Serial.print(" ");
+      }
+      for(i=0;i<3;i++)
+      {
+        for(j=0;j<4;j++)
+        {
+          ptr_uint32_buffer[j] = gps_msg[4*i+j+26];
+        }
+        NED_coordinates_accuracy[i] = (float)(uint32_buffer)*0.01;
+        //Serial.print(NED_coordinates_accuracy[i]);Serial.print(" ");
+      }
+      for(j=0;j<4;j++)
+      {
+        ptr_uint32_buffer[j] = gps_msg[j+38];
+      }
+      //Serial.print(uint32_buffer);Serial.print(" ");
+      //Serial.println(" ");
+      expected_size = 40;
+    } else if (gps_header[0] == 0x01 && gps_header[1] == 0x12 )
+    { // speed
+      Serial.readBytes(gps_msg,38);
+
+      Serial.println("speed");
+
+      for(i=0;i<3;i++)
+      {
+        for(j=0;j<4;j++)
+        {
+          ptr_int32_buffer[j] = gps_msg[4*i+j+6];
+        }
+        NED_speed[i] = ((float)(int32_buffer));
+        //Serial.print(NED_speed[i]);Serial.print(" ");
+      }
+      for(j=0;j<4;j++)
+      {
+        ptr_uint32_buffer[j] = gps_msg[j+30];
+      }
+      //Serial.print(uint32_buffer);Serial.print(" ");
+      //Serial.println(" ");
+      expected_size = 44;
+    }
+  }
+  
+  time_since_last = micros()-last_time;
+  if(time_since_last >= time_period_us)
+  {
+    Serial.print(time_since_last); Serial.print(" ");
+    if(first_update)
+    {
+      last_time = micros();
+      first_update = 0;
+    } else
+    {
+      last_time = micros();//+= time_period_us;
+    }
+    imu::Vector<3> acc=bno.getVector(Adafruit_BNO055::VECTOR_LINEARACCEL);
+    proper_acc[0] = acc.x();
+    proper_acc[1] = acc.y();
+    proper_acc[2] = acc.z();
+    
+    // quaternion
+    imu::Quaternion quat = bno.getQuat();
+    quaternion[0] = quat.w();
+    quaternion[1] = quat.x();
+    quaternion[2] = quat.y();
+    quaternion[3] = quat.z();
+
+    // Accuracy flags
+    bno.getCalibration(&sys, &gyr, &accel, &mag);
+    accuracy_mask = (uint32_t)mag + (((uint32_t)accel)<<2) + (((uint32_t)gyr)<<4) + (((uint32_t)sys)<<6);
+
+    vector_quat_proj(quaternion,proper_acc,NED_acc);
+    for(i=0;i<3;i++)
+    {
+      Serial.print(NED_coordinates[i]);Serial.print(" ");
+      Serial.print(NED_speed[i]);Serial.print(" ");
+      Serial.print(NED_acc[i]);Serial.print(" ");
+    }
+    Serial.println(" ");
+  }
+
+  
+}
--- brushless_IMU_SBG_BNO_regulation_ctseRate/brushless_IMU_SBG_BNO_regulation_ctseRate.ino
+++ brushless_IMU_SBG_BNO_regulation_ctseRate/brushless_IMU_SBG_BNO_regulation_ctseRate.ino
@@ -102,7 +102,8 @@ float linear_accel_vector[3], pos_ENU[3], posp_ENU[3], pospp_ENU[3];
 
 long t0,t1,t2,t3,t4,t5,t6,t_;
 
-const int pwmSin[] = {127, 138, 149, 160, 170, 181, 191, 200, 209, 217, 224, 231, 237, 242, 246, 250, 252, 254, 254, 254, 252, 250, 246, 242, 237, 231, 224, 217, 209, 200, 191, 181, 170, 160, 149, 138, 127, 116, 105, 94, 84, 73, 64, 54, 45, 37, 30, 23, 17, 12, 8, 4, 2, 0, 0, 0, 2, 4, 8, 12, 17, 23, 30, 37, 45, 54, 64, 73, 84, 94, 105, 116 };
+//const int pwmSin[] = {127, 138, 149, 160, 170, 181, 191, 200, 209, 217, 224, 231, 237, 242, 246, 250, 252, 254, 254, 254, 252, 250, 246, 242, 237, 231, 224, 217, 209, 200, 191, 181, 170, 160, 149, 138, 127, 116, 105, 94, 84, 73, 64, 54, 45, 37, 30, 23, 17, 12, 8, 4, 2, 0, 0, 0, 2, 4, 8, 12, 17, 23, 30, 37, 45, 54, 64, 73, 84, 94, 105, 116 };
+const int pwmSin[] = {128, 132, 136, 140, 143, 147, 151, 155, 159, 162, 166, 170, 174, 178, 181, 185, 189, 192, 196, 200, 203, 207, 211, 214, 218, 221, 225, 228, 232, 235, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 248, 249, 250, 250, 251, 252, 252, 253, 253, 253, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 254, 253, 253, 253, 252, 252, 251, 250, 250, 249, 248, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 248, 249, 250, 250, 251, 252, 252, 253, 253, 253, 254, 254, 254, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 254, 254, 254, 253, 253, 253, 252, 252, 251, 250, 250, 249, 248, 248, 247, 246, 245, 244, 243, 242, 241, 240, 239, 238, 235, 232, 228, 225, 221, 218, 214, 211, 207, 203, 200, 196, 192, 189, 185, 181, 178, 174, 170, 166, 162, 159, 155, 151, 147, 143, 140, 136, 132, 128, 124, 120, 116, 113, 109, 105, 101, 97, 94, 90, 86, 82, 78, 75, 71, 67, 64, 60, 56, 53, 49, 45, 42, 38, 35, 31, 28, 24, 21, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 8, 7, 6, 6, 5, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 5, 6, 6, 7, 8, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 24, 28, 31, 35, 38, 42, 45, 49, 53, 56, 60, 64, 67, 71, 75, 78, 82, 86, 90, 94, 97, 101, 105, 109, 113, 116, 120, 124};
 int sineArraySize;
 
 //debug
@@ -146,6 +147,8 @@ void setup() {
   
 
   sineArraySize = sizeof(pwmSin)/sizeof(int); 
+  Serial.println(" ");
+  Serial.println(sineArraySize);
 
 // Pre-computation of variables for 
   slice_angle_rd = 2*pi/(nb_pole/2.0);
@@ -278,7 +281,7 @@ int i,j,x,n;
 
     u = nominal_speed_rps/two_pi; // adding current rotation speed to PI correction
         
-    sin_amplitude = constrain(0.4+0.1*abs(u),0,1); // Modulation of amplitude vs rotation speed to work at quasi constant current
+    sin_amplitude = constrain(0.3+0.1*abs(u),0,1); // Modulation of amplitude vs rotation speed to work at quasi constant current
 
     // Transformation from rotation speed to angle increment (incrementation is done in the interuption
     motor_speed_rps = two_pi*u;//tps*two_pi;
@@ -402,7 +405,7 @@ int i,j,x,n;
 
         u = constrain(u,-U_MAX,U_MAX) + nominal_speed_rps/two_pi; // adding current rotation speed to PI correction
         
-        sin_amplitude = constrain(0.7+0.1*abs(u),0,1); // Modulation of amplitude vs rotation speed to work at quasi constant current
+        sin_amplitude = constrain(0.4+0.1*abs(u),0,1); // Modulation of amplitude vs rotation speed to work at quasi constant current
 
         // Transformation from rotation speed to angle increment (incrementation is done in the interuption
         motor_speed_rps = two_pi*u;//tps*two_pi;
